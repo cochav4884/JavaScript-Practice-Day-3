e42ea198ab49f1d2a453f8661810d5d1bde15891
Corinne Padilla <corinne padilla>
2022-05-20 01:04:46 -0700

    added mods to all files

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// CHALLENGE 1
+// Prompt the user for their first name
+const name = 'corinne';
+console.log ('corinne');
+// Store their input to a variable
+// Reverse your user’s name
+const name1 = 'enniroc';
+console.log ('enniroc');
+// alert the reversed string
+const age = '43';
+console.log ('43');
+// Challenge 2
+// Prompt for a number value (provide a default of 10)
+const num1 = 10;
+// Prompt for a second number value (provide a default of 10)
+const num2 = 5;
+// Convert the prompted values into integers using parseInt()
+console.log (`${num1} + ${num2} = ${num1 + num2}`);
+// Add the numbers together and alert the user with the result
