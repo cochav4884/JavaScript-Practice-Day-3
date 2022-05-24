4da74bcdf0f8ad08d4321e8b2cee01d59cdfe015
Corinne Padilla <corinne padilla>
2022-05-23 20:22:03 -0700

    added second js and html

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // CHALLENGE 1
 // Prompt the user for their first name
-const name = 'corinne';
-console.log ('corinne');
+const name = 'Corinne';
+console.log ('Corinne');
 // Store their input to a variable
 // Reverse your user’s name
 const name1 = 'enniroc';
@@ -17,3 +17,14 @@ const num2 = 5;
 // Convert the prompted values into integers using parseInt()
 console.log (`${num1} + ${num2} = ${num1 + num2}`);
 // Add the numbers together and alert the user with the result
+console.log('My name is ' + name + ' and I am ' + age + ' years old');
+
+const sum = num1 + num2
+console.log(`${num1} + ${num2} = ${sum}`);
+
+const confirm = confirmed ('Are you sure?');
+alert(confirm);
+
+// parseInt = number
+// parseFloat = number and decimal
+
---
+++ js/script2.js
@@ -0,0 +1,18 @@
+// CHALLENGE 1
+// Prompt the user for their first name
+const name=prompt('Enter your name', 'Corinne');
+// Store their input to a variable
+// Reverse your user’s name
+// alert the reversed string
+alert(name);
+
+// Challenge 2
+// Prompt for a number value (provide a default of 10)
+const num1= prompt('Enter a number', '10');
+// Prompt for a second number value (provide a default of 10)
+const num2= prompt('Enter another number', '10');
+// Convert the prompted values into integers using parseInt()
+const parsedNum1= parseInt(num1);
+const parsedNum2= parseInt(num2);
+// Add the numbers together and alert the user with the result
+alert(parsedNum1+parsedNum2);
